3671ab9bebf3ab9e937a03ddda4fb6890b86d5cb
Bsoulmindy <oussamabader@gmail.com>
2024-07-30 21:30:07 +0100

    Adding zoom-in & zoom-out controls

--- lib/presentation/tree/mapview/map_view.dart
+++ lib/presentation/tree/mapview/map_view.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
@@ -11,9 +13,12 @@ class MapView extends StatefulWidget {
 }
 
 class MapViewState extends State<MapView> {
+  final _mapController = MapController();
+
   @override
   Widget build(BuildContext context) {
     return FlutterMap(
+      mapController: _mapController,
       options: const MapOptions(
         initialCenter: LatLng(30.36, -9.51),
         initialZoom: 4.0,
@@ -25,6 +30,7 @@ class MapViewState extends State<MapView> {
           userAgentPackageName: 'ma.bhc.regions_music',
         ),
         RichAttributionWidget(
+          alignment: AttributionAlignment.bottomLeft,
           attributions: [
             TextSourceAttribution(
               'OpenStreetMap contributors',
@@ -33,21 +39,59 @@ class MapViewState extends State<MapView> {
           ],
         ),
         Align(
-          alignment: Alignment.bottomLeft,
+          alignment: Alignment.bottomRight,
           child: Padding(
             padding: const EdgeInsets.all(16.0),
-            child: FloatingActionButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const Zones()),
-                );
-              },
-              child: const Icon(Icons.architecture),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                // Zoom in
+                Builder(
+                  builder: (context) => FloatingActionButton(
+                    shape: const Border(bottom: BorderSide()),
+                    onPressed: () {
+                      _mapController.move(MapCamera.of(context).center,
+                          min(MapCamera.of(context).zoom + 0.5, 20));
+                    },
+                    child: const Icon(Icons.add),
+                  ),
+                ),
+
+                // Zoom out
+                Builder(builder: (context) {
+                  return FloatingActionButton(
+                    shape: Border(
+                        top: BorderSide(
+                            color: Theme.of(context).colorScheme.onSecondary)),
+                    onPressed: () {
+                      _mapController.move(MapCamera.of(context).center,
+                          max(MapCamera.of(context).zoom - 0.5, 3));
+                    },
+                    child: const Icon(Icons.remove),
+                  );
+                }),
+                const SizedBox(height: 10),
+                // Go to Zones page
+                FloatingActionButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const Zones()),
+                    );
+                  },
+                  child: const Icon(Icons.architecture),
+                ),
+              ],
             ),
           ),
         ),
       ],
     );
+  }
+
+  @override
+  void dispose() {
+    _mapController.dispose();
+    super.dispose();
   }
 }
